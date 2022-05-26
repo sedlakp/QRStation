2a350b6dc7207961ff95ea6cfc2970a8787aa518
Petr Sedlak <petrsedlak@me.com>
2022-05-26 20:09:22 +0900

    Add packages and some basic ui

---
+++ qr-station/Screens/CreateQR/CreateQRViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateQRViewController.swift
+//  qr-station
+//
+//  Created by Petr Sedlák on 25.05.2022.
+//
+
+import UIKit
+
+class CreateQRViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
