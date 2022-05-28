b7a6bf4434e0735ef638a3c08764331ef857b45f
Petr Sedlak <petrsedlak@me.com>
2022-05-28 17:06:10 +0900

    Move QR list logic to view model

---
+++ qr-station/Screens/QRList/QRListVM.swift
@@ -0,0 +1,50 @@
+//
+//  QRListVM.swift
+//  qr-station
+//
+//  Created by Petr Sedlák on 28.05.2022.
+//
+
+import Foundation
+import UIKit
+
+class QRListVM: NSObject {
+    
+    private let qrManager = QRCodeManager.shared
+    
+    var qrCodes: [QRCodeRLM] {
+        return qrManager.qrCodes
+    }
+    
+}
+
+extension QRListVM: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return qrCodes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QRCell", for: indexPath) as! QRCell
+        let qr = qrCodes[indexPath.row]
+        cell.setupCell(with: qr)
+        return cell
+    }
+   
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            // TODO: During filtering, be careful about what gets deleted
+            qrManager.delete(qrCodes[indexPath.row])
+            tableView.deleteRows(at: [indexPath], with: .fade)
+
+        }
+    }
+
+}
--- qr-station/Screens/QRList/QRListViewController.swift
+++ qr-station/Screens/QRList/QRListViewController.swift
@@ -9,10 +9,9 @@ import UIKit
 
 class QRListViewController: UIViewController {
     
-    let qrManager = QRCodeManager.shared
+    let vm = QRListVM()
 
     @IBOutlet weak var tableView: UITableView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +22,7 @@ class QRListViewController: UIViewController {
     private func tableViewSetup() {
         tableView.register(QRCell.nib, forCellReuseIdentifier: QRCell.reuseIdentifier)
         tableView.delegate = self
-        tableView.dataSource = self
+        tableView.dataSource = vm
         tableView.estimatedRowHeight = 44.0
         tableView.rowHeight = UITableView.automaticDimension
         tableView.separatorInset = .zero
@@ -36,39 +35,10 @@ class QRListViewController: UIViewController {
 }
 
 
-extension QRListViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return qrManager.qrCodes.count
-    }
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "QRCell", for: indexPath) as! QRCell
-        let qr = qrManager.qrCodes[indexPath.row]
-        cell.setupCell(with: qr)
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if (editingStyle == .delete) {
-            // TODO: During filtering, be careful about what gets deleted
-            qrManager.delete(qrManager.qrCodes[indexPath.row])
-            tableView.deleteRows(at: [indexPath], with: .fade)
-
-        }
-    }
+extension QRListViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let qr = qrManager.qrCodes[indexPath.row]
+        let qr = vm.qrCodes[indexPath.row]
         let vc = QRDetailViewController()
         vc.qr = qr
         self.present(vc, animated: true)
